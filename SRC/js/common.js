3a6c8692f6b693dcb70811c62422357e8d1058be
hiroki <202405-AP_01-nonaka@x-hide.sakurka.ne.jp>
2024-07-24 14:25:56 +0900

    #24591物件管理バリデーション

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -33,12 +33,14 @@ function fnYMDCheck(msg, obj) {
  * @return true:異常、false:正常
  */
 function isLength(length, msg, obj) {
-	rtn = false;
-	if (obj.value.length > length) {
-		alert(msg + "は" + length + "文字以内で入力して下さい");
-	}
-	return rtn;
+    rtn = false;
+    if (obj.value.length > length) {
+        alert(msg + "は" + length + "文字以内で入力して下さい");
+        rtn = true;
+    }
+    return rtn;
 }
+
 
 
 
